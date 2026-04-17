be354570e43f665f9df6a284bfb71dc9a30c8136
정예진 <yxejxnn@gmail.com>
2026-04-17 16:10:01 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/ch3scheduleappdevelop/Ch3ScheduleAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch3scheduleappdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3ScheduleAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch3ScheduleAppDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ch3scheduleappdevelop/Ch3ScheduleAppDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch3scheduleappdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3ScheduleAppDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
