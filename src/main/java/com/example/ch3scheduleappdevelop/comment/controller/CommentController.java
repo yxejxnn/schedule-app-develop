9ee18e84333342fc9cdd7d8e324dfc10c851e7e9
정예진 <yxejxnn@gmail.com>
2026-04-21 16:03:21 +0900

    fix : 댓글 생성 Controller @Valid, 세션 누락 추가

--- src/main/java/com/example/ch3scheduleappdevelop/comment/controller/CommentController.java
+++ src/main/java/com/example/ch3scheduleappdevelop/comment/controller/CommentController.java
@@ -4,6 +4,9 @@ import com.example.ch3scheduleappdevelop.comment.dto.CommentCreateRequestDto;
 import com.example.ch3scheduleappdevelop.comment.dto.CommentCreateResponseDto;
 import com.example.ch3scheduleappdevelop.comment.dto.CommentGetAllResponseDto;
 import com.example.ch3scheduleappdevelop.comment.service.CommentService;
+import com.example.ch3scheduleappdevelop.common.exception.UnauthorizedException;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +22,10 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<CommentCreateResponseDto> commentCreate(@RequestBody CommentCreateRequestDto requestDto) {
+    public ResponseEntity<CommentCreateResponseDto> commentCreate(@Valid @RequestBody CommentCreateRequestDto requestDto, HttpSession session) {
+        if (session.getAttribute("loginUser") == null) {
+            throw new UnauthorizedException();
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(commentService.save(requestDto));
     }
 
